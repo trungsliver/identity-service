79c3a1d1d17096af47fca93aecc97fde1b0bb1c4
trungsliver <buiductrung.cva@gmail.com>
2025-07-31 15:35:21 +0700

    authorization scope

--- src/main/java/com/ductrungsl/identity_service/service/AuthenticationService.java
+++ src/main/java/com/ductrungsl/identity_service/service/AuthenticationService.java
@@ -4,6 +4,7 @@ import com.ductrungsl.identity_service.dto.request.AuthenticationRequest;
 import com.ductrungsl.identity_service.dto.request.IntrospectRequest;
 import com.ductrungsl.identity_service.dto.response.AuthenticationResponse;
 import com.ductrungsl.identity_service.dto.response.IntrospectResponse;
+import com.ductrungsl.identity_service.entity.User;
 import com.ductrungsl.identity_service.exception.AppException;
 import com.ductrungsl.identity_service.exception.ErrorCode;
 import com.ductrungsl.identity_service.repository.UserRepository;
@@ -21,11 +22,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.Date;
+import java.util.StringJoiner;
 
 @Slf4j      // để dùng log
 @Service
@@ -64,7 +68,7 @@ public class AuthenticationService {
             throw new AppException(ErrorCode.UNAUTHENTICATED);
 
         // Generate token
-        var token = generateToken(request.getUsername());
+        var token = generateToken(user);
 
         return AuthenticationResponse.builder()
                 .token(token)
@@ -72,17 +76,17 @@ public class AuthenticationService {
                 .build();
     }
 
-    private  String generateToken(String username){
+    private  String generateToken(User user){
         // Xây dựng header bằng thuật toán HS512
         JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
-                .subject(username)
+                .subject(user.getUsername())
                 .issuer("ductrungsl.com")
                 .issueTime(new Date())
                 .expirationTime(new Date(
                         Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
                 ))
-                .claim("userId", "Custom")
+                .claim("scope", buildScope(user))
                 .build();
         Payload payload = new Payload(jwtClaimsSet.toJSONObject());
 
@@ -97,5 +101,12 @@ public class AuthenticationService {
             log.error("Cannot create token", e);
             throw new RuntimeException(e);
         }
+    }
+
+    private String buildScope(User user){
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        if (!CollectionUtils.isEmpty(user.getRoles()))
+            user.getRoles().forEach(stringJoiner::add);
+        return stringJoiner.toString();
     }
 }
